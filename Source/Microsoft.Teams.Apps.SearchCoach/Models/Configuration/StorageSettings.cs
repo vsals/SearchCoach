c7b6d91292ce5ece461f3021640d8ec3a89384ca
vsals <59352209+vsals@users.noreply.github.com>
2021-01-25 19:17:11 +0530

    Add files via upload

---
+++ Source/Microsoft.Teams.Apps.SearchCoach/Models/Configuration/StorageSettings.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="StorageSettings.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.SearchCoach.Models.Configuration
+{
+    /// <summary>
+    /// Class which will help to provide Microsoft Azure storage settings.
+    /// </summary>
+    public class StorageSettings
+    {
+        /// <summary>
+        /// Gets or sets Microsoft Azure storage connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
