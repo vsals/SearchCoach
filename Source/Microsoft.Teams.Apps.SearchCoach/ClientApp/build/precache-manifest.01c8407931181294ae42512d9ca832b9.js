c6ee2a111c904574e6e088de03efd3d848247ffb
vsals <59352209+vsals@users.noreply.github.com>
2021-01-27 10:21:40 +0530

    Add files via upload

---
+++ Source/Microsoft.Teams.Apps.SearchCoach/ClientApp/build/precache-manifest.01c8407931181294ae42512d9ca832b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a01e5bc3cbe7313df7f1172a882170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a3a52b42a694baf0f40",
+    "url": "/static/css/main.8074b494.chunk.css"
+  },
+  {
+    "revision": "05e34d367b6915bff68d",
+    "url": "/static/js/2.c73df6dc.chunk.js"
+  },
+  {
+    "revision": "269d5d1ae8832f5b8f27f3d569e88d96",
+    "url": "/static/js/2.c73df6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3a52b42a694baf0f40",
+    "url": "/static/js/main.62b03961.chunk.js"
+  },
+  {
+    "revision": "b4053861160ed5605e60",
+    "url": "/static/js/runtime-main.603a8db0.js"
+  },
+  {
+    "revision": "ca8a22e7073034d863cd3ddfe28dd0c4",
+    "url": "/static/media/bing-icon-yellow.ca8a22e7.png"
+  }
+]);
