40200ef55ee0c71756eb0f18edd06a5e4c6e685b
vsals <59352209+vsals@users.noreply.github.com>
2021-01-27 20:16:34 +0530

    Add files via upload

---
+++ Source/Microsoft.Teams.Apps.SearchCoach/ClientApp/src/api/bing-search-api.ts
@@ -0,0 +1,25 @@
+﻿// <copyright file="bing-search-api.ts" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+import axiosDecorator from "../api/axios-decorator";
+import { AxiosResponse } from "axios";
+
+/**
+* Get bing search results.
+* @param searchText {String} Bing search text.
+* @param selectedCountry {String} Bing search selected country.
+* @param freshness {String} Bing search selected freshness of data.
+* @param selectedDomainValues {String} Bing search selected domain values.
+*/
+export const getBingSearchResults = async (
+    searchText: string,
+    selectedCountry: string,
+    freshness: string,
+    // This value contains list of domain values with delimiter (;).
+    selectedDomainValues: string): Promise<AxiosResponse> => {
+
+    const url = `/api/search?searchText=​​​​${encodeURIComponent(searchText.trim())}&selectedCountry=​​​​${selectedCountry}&freshness=​​​​${freshness}&domainValues=${selectedDomainValues}`;
+
+    return await axiosDecorator.get(url);
+}
---
+++ Source/Microsoft.Teams.Apps.SearchCoach/ClientApp/src/models/ILeaderBoardUserData.ts
@@ -0,0 +1,9 @@
+﻿// <copyright file="ILeaderBoardUserData.ts" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+export interface ILeaderBoardUserData {
+    userName: string;
+    rightAnswers: number;
+    questionsAttempted: number;
+}
