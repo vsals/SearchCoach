5bfdb5f365ede130174ec9dc935d4e80646ebdde
vsals <59352209+vsals@users.noreply.github.com>
2021-01-28 16:09:46 +0530

    Delete Source directory

--- Source/Microsoft.Teams.Apps.SearchCoach/ClientApp/src/models/ILeaderBoardUserData.ts
+++
@@ -1,9 +0,0 @@
-﻿// <copyright file="ILeaderBoardUserData.ts" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
-// </copyright>
-
-export interface ILeaderBoardUserData {
-    userName: string;
-    rightAnswers: number;
-    questionsAttempted: number;
-}
